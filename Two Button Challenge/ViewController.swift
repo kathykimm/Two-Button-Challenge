726e0105bcf753002a5d4e30f2524467219cd89b
Kathy Kim <kitkathy5192@gmail.com>
2022-01-25 22:24:58 -0500

    Initial Commit

---
+++ Two Button Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Two Button Challenge
+//
+//  Created by Kathy Kim on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
